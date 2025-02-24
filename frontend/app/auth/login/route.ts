f2f4352989a4d9517812dce5bb03d1eaf0138435
Cleve Clayton <95036334+manOnTheMoon2101@users.noreply.github.com>
2025-02-24 21:20:21 +0200

    update route login

--- frontend/app/auth/login/route.ts
+++ frontend/app/auth/login/route.ts
@@ -1,17 +1,12 @@
 import { redirect } from "next/navigation";
-import type { NextApiResponse } from "next";
 import { cookies } from "next/headers";
 import getRequestClient from "../../lib/getRequestClient";
 
-type ResponseData = {
-  message: string;
-};
-
-export async function POST(req: Request, res: NextApiResponse<ResponseData>) {
+export async function POST(req: Request) {
   const data = await req.formData();
   const email = (data.get("email") as string) || "incognito";
   const password = (data.get("password") as string) || "password";
-
+  
   try {
     const client = await getRequestClient();
     const response = await client.auth.login({ email, password });
@@ -19,6 +14,6 @@ export async function POST(req: Request, res: NextApiResponse<ResponseData>) {
   } catch (error) {
     console.error("Error logging in", error);
   }
-
+  
   return redirect("/");
 }
