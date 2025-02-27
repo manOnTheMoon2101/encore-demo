627a0657c5dd3c1af7fb63a2698c6088391da96b
Cleve Clayton <95036334+manOnTheMoon2101@users.noreply.github.com>
2025-02-27 20:35:30 +0200

    config removed

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -5,7 +5,7 @@ import { neon } from "@neondatabase/serverless";
 import { config } from "dotenv";
 
 
-config({ path: ".env" });
+// config({ path: ".env" });
 
 const sql = neon(process.env.DATABASE_URL!);
 
